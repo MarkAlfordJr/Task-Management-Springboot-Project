878bf5be95338f1dae2f2df728f9a384066ae136
Mark Alford Jr <mark.alford0424@gmail.com>
2026-07-03 19:46:44 -0400

    initial file generation from spring.io is completed. gitignore file is included.

---
+++ src/test/java/com/example/Task_Management_Project/TaskManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Task_Management_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
